c9a719a1b910276ee36d2e4421c5f9970a5812e0
Julian <julest@bu.edu>
2018-02-24 14:52:32 -0500

    Finished random interval fortune uploads

--- 2Lab/sheetsAutoWriter.py
+++ 2Lab/sheetsAutoWriter.py
@@ -1,20 +1,37 @@
 import subprocess
-import os
-import serial
 import time
-import signal
 import gspread
+from random import randint
 from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime
+
+SECONDS_IN_A_MINUTE = 60
 
 scope = ['https://spreadsheets.google.com/feeds']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
 gc = gspread.authorize(credentials)
 wks = gc.open('Lab2-GSheet').sheet1
 
-for r in range (1,5):
-	print (' '*40)
-	print ('Row: %d' % r)
-	for c in range (1,5):
+def hasBeenThirtySeconds(lastPrintTime):
+    if (time.time() - lastPrintTime >= 30):
+        return True
+
+    return False
+
+rowCount = 1
+lastPrint = time.time()
+duration = time.time() + (SECONDS_IN_A_MINUTE*3)
+
+while time.time() < duration:
+	if (randint(0,10000) is randint(0,5000) or hasBeenThirtySeconds(lastPrint)):
 		fortuneCall = subprocess.Popen(['fortune'], stdout=subprocess.PIPE)
-		wks.update_cell(r,c, fortuneCall.stdout.readlines())
-		print ("[",r,",",c,"]: ",wks.cell(r,c).value)
+		fortuneMsg = fortuneCall.stdout.readlines()
+		timeStamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+		
+		print (timeStamp, fortuneMsg)
+		wks.update_cell(rowCount, 1, timeStamp)
+		wks.update_cell(rowCount, 2, fortuneMsg)
+		
+		rowCount = rowCount + 1
+		lastPrint = time.time()
+
